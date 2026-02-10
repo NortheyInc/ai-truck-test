23ae8d5956d005bbeb0ab58c73014eb50e8dcecd
NortheyInc <northey_pete@live.com.au>
2026-02-10 18:41:08 +1000

    Add initial deliveries data structure

--- deliveries_data.py
+++ deliveries_data.py
@@ -1,1 +1,6 @@
+deliveries = [
+    {"delivery_id": 1, "weight": 50, "volume": 1, "coords": (-33.870000, 151.220000)},
+    {"delivery_id": 2, "weight": 70, "volume": 2, "coords": (-33.880000, 151.210000)},
+    # ... add all deliveries for the day
+]
 
