70c985a1cd2782a4a6ba5fe1b2d46bd1838e0156
NortheyInc <northey_pete@live.com.au>
2026-02-10 18:40:48 +1000

    Initialize trucks data with specifications
    Added initial data for trucks with their specifications.

--- trucks_data.py
+++ trucks_data.py
@@ -1,1 +1,6 @@
+trucks = [
+    {"truck_id": 1, "max_weight": 1000, "max_volume": 15, "depot": (-33.865143, 151.209900)},
+    {"truck_id": 2, "max_weight": 1200, "max_volume": 20, "depot": (-33.865143, 151.209900)},
+    # ... add all 150 trucks here
+]
 
